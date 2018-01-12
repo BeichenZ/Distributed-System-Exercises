77251db8c37a834d732dc867a8db5a1a9a721841
BC <zhang.beichen@dhs.sg>
2018-01-12 01:52:36 -0800

    Implemented UDP communication.However, Read function has one bug to be fixed.

--- HW1/client.go
+++ HW1/client.go
@@ -64,20 +64,61 @@ type FortuneMessage struct {
 func main() {
 	//Parse Command line Input
 	if len(os.Args) != 4 {
-		fmt.Println("Invalid Number Command line Arguments")
+		fmt.Println("Invalid Number of Command line Arguments")
 		os.Exit(1)
 	}
-	udpAddr_Local,_ := net.ResolveUDPAddr("udp",os.Args[1])
-	tcpAddr_Local,_ := net.ResolveTCPAddr("tcp",os.Args[2])
-	udpAddr_Aserver,_ := net.ResolveUDPAddr("udp",os.Args[3])
+	udpAddr_Local,err := net.ResolveUDPAddr("udp",os.Args[1])
+	CheckError(err)
+	tcpAddr_Local,err := net.ResolveTCPAddr("tcp",os.Args[2])
+	//CheckError(err)
+	udpAddr_Aserver,err := net.ResolveUDPAddr("udp",os.Args[3])
+	CheckError(err)
+	
+	fmt.Println("Local_UDP,Local_TCP,Target_UDP are",udpAddr_Local,tcpAddr_Local,udpAddr_Aserver)
+	
+	//establish UDP connection
+	udp_Conn,err := net.DialUDP("udp",udpAddr_Local,udpAddr_Aserver)
+	CheckError(err)
+	defer udp_Conn.Close()
+	
+	//Write an aribitary message to UDP Server
+	randomMsg:= make([]byte,1024)
+	_,err = udp_Conn.Write(randomMsg)
+	CheckError(err)
+	
+	//Receive Message From Server
+	 fmt.Println("i am runnign")
+     n, addr, err := udp_Conn.ReadFromUDP(randomMsg)
+	 CheckError(err)
+     fmt.Println("UDP Server : ", addr)
+     fmt.Println("Received from UDP server : ", string(randomMsg[:n]))
+	/*for {
+		fmt.Println("i am runnign")
+		rxMsg := make([]byte,100)
+		n,err := udp_Conn.Read(rxMsg)
+		CheckError(err)
+		fmt.Println("Received From A server:",string(rxMsg[:n]))
+		
+	}*/
+	
+	fmt.Println("This is Done")
 
 
-	fmt.Println(udpAddr_Local,tcpAddr_Local,udpAddr_Aserver)
-	fmt.Println("This is the end")
+	//fmt.Println("UDP Server:",addr)
+	//fmt.Println("Received From A server:",string(buffer[:n]))
+	
 		
 	// Use json.Marshal json.Unmarshal for encoding/decoding to servers
 
 }
+
+func CheckError(err error){
+	if err != nil {
+		fmt.Println("Error Ocurred:",err)
+		os.Exit(0)
+	}
+}
+
 
 // Returns the MD5 hash as a hex string for the (nonce + secret) value.
 func computeNonceSecretHash(nonce string, secret string) string {
