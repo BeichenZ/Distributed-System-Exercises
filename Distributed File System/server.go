affd0060ca1a05b65a16bae520258c6d9313e7ab
Beichen Zhang <j2y8@anvil.ugrad.cs.ubc.ca>
2018-01-25 21:24:39 -0800

    Set up simple test server

--- Distributed File System/server.go
+++ Distributed File System/server.go
@@ -5,13 +5,13 @@ import (
 	//"encoding/hex"
 
 	// TODO
-	"encoding/json"
+	//"encoding/json"
 	"fmt"
-	"log"
-	"math/rand"
+	//"log"
+	//"math/rand"
 	"net"
 	"os"
-	"time"
+	//"time"
 )
 
 //Main Method
@@ -21,14 +21,30 @@ func main() {
 		os.Exit(1)
 	}
 
-	tcpAddr_Server,err := net.ResolveTCPAddr("tcp",os.Args[1])
-	Check_ServerError(err)
+	tcpAddr_Server := os.Args[1]
 	//Server's Overall Structure is referenced from https://coderwall.com/p/wohavg/creating-a-simple-tcp-server-in-go
 
 	//Listen for incoming connections
 	Iconn,err := net.Listen("tcp",tcpAddr_Server)
 	Check_ServerError(err)
+	defer Iconn.Close()
+	fmt.Println("Listening on" + tcpAddr_Server)
+	//main loop
+	for{
+		conn,err := Iconn.Accept()
+		Check_ServerError(err)
+		go handleRequest(conn)
+	}
 
+}
+//Separate Thread to handle the request
+func handleRequest(conn net.Conn){
+	buf := make([]byte,1024)
+	reqLen,err := conn.Read(buf)
+	fmt.Println("message from client:",buf[:reqLen])
+	Check_NonFatalError(err)
+	conn.Write([]byte("Message Received."))
+	conn.Close()
 }
 
 //Check for Server's error that leads to Server Shut-Down
@@ -36,5 +52,10 @@ func Check_ServerError(err error) {
 	if err != nil {
 		fmt.Println("Error Ocurred:", err)
 		os.Exit(0)
+	}
+}
+func Check_NonFatalError(err error){
+	if err !=nil {
+		fmt.Println("Non-Fatal Error:",err)	
 	}
 }
